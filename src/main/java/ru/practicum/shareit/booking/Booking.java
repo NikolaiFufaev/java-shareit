db18cbe3bb318f914b216acbc767ccbc9574e5b2
NikolaiFufaev <98405572+NikolaiFufaev@users.noreply.github.com>
2022-08-20 17:34:21 +0300

    тесты проходят, вроде это все что требовалось

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,18 @@
 package ru.practicum.shareit.booking;
 
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import java.time.LocalDateTime;
+
 /**
  * // TODO .
  */
 public class Booking {
+    long id;
+    LocalDateTime start;
+    LocalDateTime end;
+    Item item;
+    User booker;
+    String status;
 }
--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,32 @@
 package ru.practicum.shareit.item.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.practicum.shareit.requests.ItemRequest;
+import ru.practicum.shareit.user.model.User;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 /**
  * // TODO .
  */
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
+    private Long id;
+    @NotEmpty
+    private String name;
+    @NotEmpty
+    private String description;
+
+    @NotNull
+    private Boolean available;
+    private User owner;
+    private ItemRequest request;
 }
